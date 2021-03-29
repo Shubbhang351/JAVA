fbf17e5892458709154f2b68e104a805860e0b6b
Shubhang <shubhang.verma_cs18@gla.ac.in>
2021-03-29 11:57:23 +0530

    351

---
+++ LeetCode/Backspace_String_Compare.py
@@ -0,0 +1,27 @@
+def solve(s1,s2):
+    s3 = ""
+    s4 = ""
+    t = 0
+    for i in range(len(s1) - 1, - 1, -1):
+        if s1[i] == '#':
+            t += 1
+        else:
+            if t > 0:
+                t -= 1
+            else:
+                s3 = s1[i] + s3
+    t = 0
+    for i in range(len(s2) - 1, - 1, -1):
+        if s2[i] == '#':
+            t += 1
+        else:
+            if t > 0:
+                t -= 1
+            else:
+                s4 = s2[i] + s4
+    print(s3)
+    print(s4)
+
+s1 = input()
+s2 = input()
+solve(s1,s2)

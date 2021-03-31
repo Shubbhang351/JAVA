54ae9173ddd1da1ecc2de1d6c89885fa4bb93bf0
Shubhang <shubhang.verma_cs18@gla.ac.in>
2021-03-31 20:00:50 +0530

    351

--- back_traking/coin_change.py
+++ back_traking/coin_change.py
@@ -1,11 +1,11 @@
-def coinChangeCombination(denom, amount, lastDenomIndex, ans):
+def coinChange_Combination_amount_respect(denom, amount, lastDenomIndex, ans):
     if amount == 0:
         print(ans)
         return
 
     for i in range(lastDenomIndex, len(denom)):
         if amount >= denom[i]:
-            coinChangeCombination(denom, amount - denom[i], i, ans + str(denom[i]))
+            coinChange_Combination_amount_respect(denom, amount - denom[i], i, ans + str(denom[i]))
 
 def coinChangePermutation(denom, amount, ans):
     if amount == 0:
@@ -16,10 +16,32 @@ def coinChangePermutation(denom, amount, ans):
         if denom[i] <= amount:
             coinChangePermutation(denom, amount - denom[i] , ans + str(denom[i]))
 
+
+
+def coinChange_Combination_coin_respect(denom, amount, i, ans):
+    # positive base case
+    if amount == 0:
+        print(ans)
+        return
+    
+    # negative base case
+    if amount < 0 or i >= len(denom):
+        return
+    
+    # current (i th) coin can contribute in amount
+    coinChange_Combination_coin_respect(denom, amount - denom[i], i, ans + str(denom[i]) + ' ')
+
+    # current (i th) coin can not contribute in amount
+    coinChange_Combination_coin_respect(denom, amount, i + 1, ans)
+
+    
+
 denom = [2,3,5]
 amount = 7
 lastDenomIndex = 0
 ans = ''
-coinChangeCombination(denom, amount, lastDenomIndex, ans)
+coinChange_Combination_amount_respect(denom, amount, lastDenomIndex, ans)
 print("----------------")
 coinChangePermutation(denom, amount, ans)
+print("\n------------\n")
+coinChange_Combination_coin_respect(denom, amount, 0, "")
